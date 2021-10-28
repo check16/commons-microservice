1df9934537ba5843116ff02eea179e50cc8d035a
Antonio Sanchez <antonio.san.87@hotmail.com>
2021-10-28 19:18:51 +0200

    Added validation and pagination

--- src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
@@ -2,10 +2,15 @@ package com.formacionbdi.microservicios.commons.controllers;
 
 import com.formacionbdi.microservicios.commons.services.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class CommonController<E, S extends CommonService<E>> {
@@ -16,6 +21,11 @@ public class CommonController<E, S extends CommonService<E>> {
     @GetMapping
     public ResponseEntity<?> listar() {
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/pageable")
+    public ResponseEntity<?> listar(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 
     @GetMapping("/{id}")
@@ -29,7 +39,10 @@ public class CommonController<E, S extends CommonService<E>> {
     }
 
     @PostMapping
-    public ResponseEntity<?> crear(@RequestBody E entity) {
+    public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
+        if (result.hasErrors()) {
+            return this.validar(result);
+        }
         E entityDB = service.save(entity);
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(entityDB);
@@ -40,5 +53,16 @@ public class CommonController<E, S extends CommonService<E>> {
         service.deleteById(id);
         return ResponseEntity.noContent()
                              .build();
+    }
+
+    protected ResponseEntity<?> validar(BindingResult result) {
+        Map<String, Object> errores = new HashMap<>();
+        result.getFieldErrors()
+              .forEach(err -> {
+                  errores.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+              });
+
+        return ResponseEntity.badRequest()
+                             .body(errores);
     }
 }
--- src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
@@ -1,12 +1,14 @@
 package com.formacionbdi.microservicios.commons.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E> {
+public class CommonServiceImpl<E, R extends PagingAndSortingRepository<E, Long>> implements CommonService<E> {
 
     @Autowired
     protected R repository;
@@ -15,6 +17,11 @@ public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements 
     @Transactional(readOnly = true)
     public Iterable<E> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public Page<E> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
     @Override
